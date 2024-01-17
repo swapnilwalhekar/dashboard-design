7188febbdac49fbe6e0e3c67a454eea401e78548
Swapnil Walhekar <108648658+swapnilwalhekar@users.noreply.github.com>
2024-01-17 18:45:22 +0530

    PC uploaded
    Designing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import Home from './Home'
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Sidebar from "./Sidebar";
 
 function App() {
-  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
-
-  const OpenSidebar = () => {
-    setOpenSidebarToggle(!openSidebarToggle)
-  }
+  const [openSidebar, setOpenSidebar] = useState(false);
+  const toggleSidebar = () => {
+    setOpenSidebar(!openSidebar);
+  };
 
   return (
-    <div className='grid-container'>
-      <Header OpenSidebar={OpenSidebar}/>
-      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-      <Home />
-    </div>
-  )
+    <>
+      <div className="grid-container">
+        <Header toggleSidebar={toggleSidebar} />
+        <Sidebar openSidebar={openSidebar} toggleSidebar={toggleSidebar} />
+        <Home />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import 
- {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
- from 'react-icons/bs'
+// import React from "react";
+import {
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsJustify,
+  BsPersonCircle,
+  BsSearch,
+} from "react-icons/bs";
 
-function Header({OpenSidebar}) {
+const Header = ({ toggleSidebar }) => {
   return (
-    <header className='header'>
-        <div className='menu-icon'>
-            <BsJustify className='icon' onClick={OpenSidebar}/>
-        </div>
-        <div className='header-left'>
-            <BsSearch  className='icon'/>
-        </div>
-        <div className='header-right'>
-            <BsFillBellFill className='icon'/>
-            <BsFillEnvelopeFill className='icon'/>
-            <BsPersonCircle className='icon'/>
-        </div>
+    <header className="header">
+      <div className="menu-icon">
+        <BsJustify className="icon" onClick={toggleSidebar} />
+      </div>
+      <div className="header-left">
+        <BsSearch className="icon" />
+      </div>
+      <div>
+        <BsFillBellFill className="icon" />
+        <BsFillEnvelopeFill className="icon" />
+        <BsPersonCircle className="icon" />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
